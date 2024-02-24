b80c4f44e65084d2c0c6a2555a190797e766af94
Nathan Larson <57366429+xenloops@users.noreply.github.com>
2024-02-24 09:12:25 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
+?>
 <div id="footer">
 
 Copyright &#169; 2004-<?php echo date("Y") ?> Nathan Larson.
-		<i>Product names are registered trademarks of their respective trademark
-		holders. Usage here is not intended as an infringement or endorsement.</i>
+	<i>Product names are registered trademarks of their respective trademark
+	holders. Usage here is not intended as an infringement or endorsement.</i>
 
 </div>
+<?php
