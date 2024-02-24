8a1ceb5d1cd4a41c6c877ebb2f3fcdd7eefdcc61
Nathan Larson <57366429+xenloops@users.noreply.github.com>
2024-02-24 09:02:36 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+
+Copyright &#169; 2004-<?php echo date("Y") ?> Nathan Larson.
+		<i>Product names are registered trademarks of their respective trademark
+		holders. Usage here is not intended as an infringement or endorsement.</i>
+
+</div>
